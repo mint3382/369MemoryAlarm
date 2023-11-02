b02d3be60acb05d09b3e92f3053755a0a9d46a31
mint3382 <mskim0926@naver.com>
2023-11-02 13:56:54 +0900

    feat: 프로젝트 파일 생성

---
+++ 369MemoryAlarm/369MemoryAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  369MemoryAlarm
+//
+//  Created by minsong kim on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
